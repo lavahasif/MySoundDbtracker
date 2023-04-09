bb7e578c17115065626012d66fac82f6f3631ae1
Hasif <Hasif@HASIF>
2023-04-09 22:37:25 +0400

    Add project files.

---
+++ MySoundDbtracker/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace MySoundDbtracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(49, 70);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(364, 58);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Quicksand", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(218, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 59);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Quicksand", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 59);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MaxValue :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 204);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Font = new System.Drawing.Font("Quicksand", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Find dB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MySoundDbtracker/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace MySoundDbtracker
+{
+
+
+    public partial class Form1 : Form
+    {
+        int MaxValue = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            var waveIn = new WaveInEvent();
+            waveIn.DataAvailable += OnDataAvailable;
+            waveIn.StartRecording();
+
+
+
+            //waveIn.StopRecording();
+
+        }
+        int bufferSize = 44100; // 1 second of audio data at 44100 Hz
+        int samplingRate = 44100;
+        int bufferIndex = 0;
+        private void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            
+
+            // Initialize variables for storing the audio data and maximum value
+            byte[] audioBuffer = new byte[bufferSize * 4];
+
+            // calculate the RMS amplitude
+            float rms = 0;
+            for (int i = 0; i < e.BytesRecorded; i += 2)
+            {
+                short sample = (short)((e.Buffer[i + 1] << 8) | e.Buffer[i]);
+                rms += sample * sample;
+            }
+            rms = (float)Math.Sqrt(rms / (e.BytesRecorded / 2));
+
+            // calculate the dB level
+            float dB = 20 * (float)Math.Log10(rms);
+
+            // Add the new data to the audio buffer
+            short sampleValue = (short)(dB * short.MaxValue);
+            byte[] sampleBytes = BitConverter.GetBytes(sampleValue);
+            Array.Copy(sampleBytes, 0, audioBuffer, bufferIndex, sampleBytes.Length);
+            bufferIndex += sampleBytes.Length;
+
+            
+
+
+
+            int dB1 = (int)dB;
+            if (dB1 >= MaxValue && bufferIndex >= 2 * bufferSize * 4)
+            {
+                saveBuffer(audioBuffer);
+                bufferIndex = 0;
+            }
+                
+            progressBar1.Invoke(new Action(() =>
+            {
+                progressBar1.Value = (int)dB;
+                label1.Text = dB1.ToString();
+                if (dB1 > MaxValue)
+                {
+
+                    MaxValue = dB1;
+                    label2.Text = "MaxValue: " + dB1.ToString();
+                }
+            }
+
+            )); ;
+
+
+        }
+
+        void saveBuffer(byte[] buffer)
+        {
+            // Create a new WaveFileWriter object to write the audio data to a file
+            WaveFileWriter waveFileWriter = new WaveFileWriter($"outpu2t.wav", new WaveFormat(44100, 16, 2));
+
+            // Write the audio data from the buffer to the WaveFileWriter object
+            
+            waveFileWriter.Write(buffer, 0, bufferIndex);
+            waveFileWriter.Close();
+        }
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
